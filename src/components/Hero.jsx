2b4543fc057ac5cd41b1a89c38f0b203dfcf9a32
Priyanshu14G <pgupta0628@gmail.com>
2024-08-01 17:27:06 +0530

    second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
             variants={container(0)}
             initial = "hidden"
             animate = "visible"
-              className="px-6 pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+              className="px-6 pb-16 text-3xl font-thin tracking-tight lg:mt-16 lg:text-7xl"
             >
               Priyanshu Gupta
             </motion.h1>
